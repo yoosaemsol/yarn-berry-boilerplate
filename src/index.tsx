830aa14eb1096d3f5e8f944dc30939b9231e074a
yoosaemsol <yoosaemsol@gmail.com>
2024-06-20 14:34:30 -0400

    feat: global styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+import { Global } from '@emotion/react'
+import globalStyles from '@styles/globalStyles'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
 )
